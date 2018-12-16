2a0b7fb1db46944b0d9e8b37d71c9dae72eb3082
Mathias Hoppe <hoppe@chalmers.se>
2018-12-16 15:13:37 +0100

    Add transparent colormap

--- SyntheticImage.py
+++ SyntheticImage.py
@@ -14,7 +14,7 @@ from PolarizedImage import ImageType, PolarizedImage
 
 class SyntheticImage:
     
-    def __init__(self, figure=None, canvas=None, registerGeriMap=True):
+    def __init__(self, figure=None, canvas=None, registerGeriMap=True, gerimapTransparencyThreshold=None):
         # PROPERTIES
         self.canvas = canvas
         self.captions = []
@@ -63,7 +63,7 @@ class SyntheticImage:
         self.axes = None
 
         if registerGeriMap:
-            SyntheticImage.registerGeriMap()
+            SyntheticImage.registerGeriMap(transparencyThreshold=gerimapTransparencyThreshold)
 
     #####################################################
     #
@@ -245,15 +245,39 @@ class SyntheticImage:
         except KeyError: pass
 
     @staticmethod
-    def registerGeriMap():
+    def registerGeriMap(transparencyThreshold=0.4):
         """
         Register the perceptually uniform colormap 'GeriMap' with matplotlib
+
+        transparencyThreshold: All intensities below this threshold will have
+                               a non-zero alpha value (making them more or
+                               less transparent). The opacity varies linearly,
+                               being 1 at this value, and 0 at zero.
         """
         gm = [(0, 0, 0), (.15, .15, .5), (.3, .15, .75),
               (.6, .2, .50), (1, .25, .15), (.9, .5, 0),
               (.9, .75, .1), (.9, .9, .5), (1, 1, 1)]
         gerimap = LinearSegmentedColormap.from_list('GeriMap', gm)
         gerimap_r = LinearSegmentedColormap.from_list('GeriMap_r', gm[::-1])
+
+        if transparencyThreshold is not None:
+            n = int(gerimap.N * transparencyThreshold)
+            nn = gerimap.N - n
+
+            if n < 0 or nn < 0:
+                raise ValueError('Transparency threshold must be a value between 0 and 1.')
+
+            a = np.linspace(0, 1, n)
+            b = np.ones((nn,))
+
+            gmap = gerimap(np.arange(gerimap.N))
+            gmap[:,-1] = np.concatenate((a,b), axis=None)
+            gerimap = LinearSegmentedColormap.from_list('GeriMap', gmap)
+
+            gmap_r = gerimap_r(np.arange(gerimap.N))
+            gmap_r[:,-1] = np.concatenate((a,b), axis=None)
+            gerimap_r = LinearSegmentedColormap.from_list('GeriMap_r', gmap_r)
+
         plt.register_cmap(cmap=gerimap)
         plt.register_cmap(cmap=gerimap_r)
 
